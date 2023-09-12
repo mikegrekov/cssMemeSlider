b28e987091e3a2cd37fbc46edcde169b35470e43
mikegrekov <svg200914@gmail.com>
2023-09-12 19:42:03 +0100

    feat: implemented slider, buttons and title for destop  (Tue, Sep 12, 2023 19:41:48 PM)

---
+++ cssMemeSlider/carusel.js
@@ -0,0 +1,32 @@
+class_list = {
+    CARUSEL_BUTTON: ".carusel-item",
+    CARUSEL_TITLES: ".titles-carusel",
+    CARUSEL_IMAGE: ".image-item",
+    CARUSEL_CONTAINER: ".images",
+}
+const carusel = document.querySelectorAll(class_list.CARUSEL_BUTTON);
+const caruselImage = document.querySelectorAll(class_list.CARUSEL_IMAGE);
+const caruselTitle = document.querySelector(class_list.CARUSEL_TITLES);
+const caruselImageContainerWidth = document.querySelector(class_list.CARUSEL_CONTAINER).clientWidth + 25;
+
+activateSlide(0);
+
+function activateSlide(n) {
+
+
+    activeImage = n;
+
+    for (let i = 0; i < carusel.length; i++) {
+        carusel[i].classList.remove("carusel-active");
+    };
+    
+    moveImages(n);
+    carusel[n].className += " carusel-active";
+    caruselTitle.innerHTML = caruselImage[n].children[0].alt;
+}
+
+function moveImages(n) {
+    for (let i = 0; i < caruselImage.length; i++) {
+        caruselImage[i].style.transform = `translateX(${-caruselImageContainerWidth * n}px)`;
+    }
+}
